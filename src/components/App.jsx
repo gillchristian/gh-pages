86f4beddd2af84dd71b3eb2d32c272ecfe419bfd
gillchristian <gillchristiang@gmail.com>
2016-06-17 03:51:56 -0300

    Add a Header to de page

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -2,15 +2,15 @@ import React from 'react'
 
 import ReposList from '../containers/repos/ReposList.container'
 import SearchBar from './search-bar/SearchBar'
-import Footer from './footer/Footer'
+import Footer from './layout/Footer'
+import Header from './layout/Header'
 import Loading from './common/Loading'
 import Error from './common/Error'
-
-import 'whatwg-fetch'
 
 const App = ({error, loading}) => (
   <div className="Site">
     <main>
+      <Header />
       <div className="container">
         <SearchBar />
         {
