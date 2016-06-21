0d8dc28923ac87ddd5eeb9326bfb2166ebf4e107
gillchristian <gillchristiang@gmail.com>
2016-06-21 20:27:35 -0300

    Fix bug were repo request error state was persisting after success

--- src/reducers/repos.js
+++ src/reducers/repos.js
@@ -24,6 +24,7 @@ export default function (state = INITIAL_STATE, action) {
       return {
         ...state,
         loading: false,
+        error: false,
         list: action.payload
       }
     case REQUEST_REPOS_ERROR:
